d0a52d72e139b9dffefc99b941c1fe9cf1dd5b8f
Jennifer Ramirez <jxr20920@dexcom.com>
2022-03-29 18:46:10 -0600

    init commit

---
+++ src/test/java/BrowserNavigationTests.java
@@ -0,0 +1,15 @@
+import enums.WebDriverType;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class BrowserNavigationTests extends BaseTest {
+    final String url = "https://www.google.com";
+    private WebDriver _childWebDriver = getWebDriver(WebDriverType.CHROME);
+
+    @Test
+    public void testOpenBrowser() {
+        //Metodo para abrir URL con Selenium WebDriver
+        _childWebDriver.get(url);
+        _childWebDriver.manage().window().maximize();
+    }
+}
