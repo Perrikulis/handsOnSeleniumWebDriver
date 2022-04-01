f6573d6bbb5eb2bfcce1a11c7e7226116b667a1f
SERGIO-ASUS-LAP\Sergio <saterladgodxtrem@gmail.com>
2022-03-31 22:28:14 -0600

    Se agrega lo realizado por mi, además de incluir la tarea del 30 de marzo.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,16 +5,25 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import org.testng.annotations.*;
+
+import java.io.File;
 
 public class BaseTest {
 
-    //Comienzo: Declarando variables Globales
-    private String _chromeDriverPath = "/Users/jxr20920/Downloads/chromedriver";
-    private String _fireFoxDriverPath = "/Users/jxr20920/Downloads/geckodriver";
-    private String _operaDriverPath = "/Users/jxr20920/Downloads/operaDriver";
-    //En Windows el path debe empezar con C: y terminar el chromedriver.exe
+/*   public void recibeDiaDeLaSemana(DaysOfWeek daysOfWeek){
+
+    }
+
+    public void ejemplo(){
+       recibeDiaDeLaSemana(DaysOfWeek.DOMINGO);
+    }
+*/
+
+    //Declarando Variables Globales
+    private String _chromeDriverPath = "C:\\DChrome\\chromedriver.exe";
+    private String _firefoxDriverPath = "C:\\DChrome\\geckodriver.exe";
+    private String _operaDriverPath = "C:\\DChrome\\geckodriver.exe";
 
     public WebDriver webDriver = getWebDriver(WebDriverType.CHROME);
 
@@ -22,46 +31,51 @@ public class BaseTest {
 
     //Fin Variables Globales
 
-    private WebDriver getWebDriver(WebDriverType webDriverType) {
+    //Creación de Métodos
+    public WebDriver getWebDriver(WebDriverType webDriverType){
         this._webDriverType = webDriverType;
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-
         //Start SWITCH
-        switch (webDriverType) {
+        switch (webDriverType){
             case CHROME:
-                System.setProperty("webdriver.chrome.driver", _chromeDriverPath);
+                System.setProperty("webdriver.chrome.driver",_chromeDriverPath);
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--incognito");
-                desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+                //opciones de chrome se cargan en objeto desiredCapabilities donde se indica que
+                //se use chrome con las opciones especificadas
+                desiredCapabilities.setCapability(ChromeOptions.CAPABILITY,options);
                 options.merge(desiredCapabilities);
                 webDriver = new ChromeDriver(options);
                 break;
             case FIREFOX:
-                System.setProperty("webdriver.gecko.driver", _fireFoxDriverPath);
+                System.setProperty("webdriver.gecko.driver", _firefoxDriverPath);
                 webDriver = new FirefoxDriver();
                 break;
             case OPERA:
-                //Will not work for now!!!!!!
+                //Don't work for now
                 System.setProperty("webdriver.opera.driver", _operaDriverPath);
                 webDriver = new OperaDriver();
                 break;
-        } // Ends SWITCH
+        } //END SWITCH
 
-        System.out.println("El WebDriver tipo " + webDriverType + " se ha inicializado con exito.");
+        System.out.println("El WebDriver tipo "+ webDriverType + " se ha inicializado con éxito.");
         return webDriver;
     }
 
     //Annotations
     @BeforeTest
-    public void beforeTest() {
-        System.out.println("Before test en Clase BaseTest.java");
+    public void beforeTest(){
+        System.out.println("Before test en Clase BeforeTest.java");
+        //webDriver = getWebDriver(WebDriverType.CHROME);
     }
 
     @AfterTest
-    public void afterTest() {
-        System.out.println("After test en Clase BaseTest.java");
-        //Muere WebDriver
-        webDriver.quit();
+    public void afterTest(){
+        System.out.println("After test en Clase BeforeTest.java");
+        //Se muere el WebDriver
+
+        webDriver.quit();  //Cierra el navegador
+        //webDriver.close(); //Cierra la pestaña
     }
 
 }
--- src/test/java/MyStoreHomeTests.java
+++ src/test/java/MyStoreHomeTests.java
@@ -5,83 +5,108 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class MyStoreHomeTests extends BaseTest {
-    final String urlMyStore = "http://automationpractice.com/index.php";
-    private WebDriver _childWebDriver = webDriver;
+public class MyStoreHomeTests extends BaseTest{
 
-    //Localizadores o Selectors
-    final String logoCSSLocatorFalse = "div.header_logo";
+    private WebDriver _childWebDriver = webDriver;
+    final String urlMyStore = "http://automationpractice.com/index.php";
+
+    //Localizadores o selectores
     final String logoCSSLocator = "div#header_logo";
-    final String cartCSSLocator = "div.shopping_cart > a";
+    final String logoCSSLocatorFalso = "div.header_logo";
+    final String carritoCSSLocator = "div.shopping_cart > a";
+    final String searchTextBoxLocator = "div > form#searchbox";
 
     @Test
-    public void testSearchFirstElement() {
+    public void testSearchFirstElement(){
         openMyStore();
         WebElement logo = _childWebDriver.findElement(By.cssSelector(logoCSSLocator));
         System.out.println(logo.isDisplayed());
     }
 
     @Test
-    public void testManejoDeException() {
+    public void testExceptionManagement(){
         openMyStore();
         try {
-            //Ejemplo
-            WebElement logo = _childWebDriver.findElement(By.cssSelector(logoCSSLocatorFalse));
-            System.out.println("Codigo try: " + logo.isDisplayed());
-        } catch (NoSuchElementException logoNoLocalizado) {
-            System.out.println("Catch logoNoLocalizado: El localizador " + logoCSSLocatorFalse + " no funciona.");
+            WebElement logo = _childWebDriver.findElement(By.cssSelector(logoCSSLocatorFalso));
+            System.out.println("Código try: " + logo.isDisplayed());
+        }
+        //Se utiliza NoSuchElementException de selenium
+        catch (NoSuchElementException logoNoLocalizado){
+            System.out.println("Catch logo no localizado: El localizador " + logoCSSLocatorFalso + " no funciona.");
             WebElement logo = _childWebDriver.findElement(By.cssSelector(logoCSSLocator));
-            System.out.println("El logo se esta mostrando: " + logo.isDisplayed());
+            System.out.println("El logo se está mostrando: " + logo.isDisplayed());
         }
     }
 
     @Test
-    public void testManejoDeException2() {
+    public void testExceptionManagement2(){
         openMyStore();
         try {
             WebElement logo = _childWebDriver.findElement(By.cssSelector(logoCSSLocator));
             Assert.assertTrue(logo.isDisplayed(), "Mensaje de error");
             System.out.println("El elemento fue localizado usando: " + logoCSSLocator);
-        } catch (NoSuchElementException elementoNoLocalizado) {
-            Assert.fail("El elemento no fue encontrado con el localizador: " + logoCSSLocatorFalse);
+        }catch (NoSuchElementException elementoNoLocalizado){
+            Assert.fail("El elemento no fue encontrado con el localizador: " + logoCSSLocatorFalso);
         }
     }
 
     @Test
-    public void testManejoDeException2Fails() {
+    public void testExceptionManagement2Fails(){
         openMyStore();
         try {
-            WebElement logo = _childWebDriver.findElement(By.cssSelector(logoCSSLocatorFalse));
+            WebElement logo = _childWebDriver.findElement(By.cssSelector(logoCSSLocatorFalso));
+            //logo.click();
             Assert.assertTrue(logo.isDisplayed(), "Mensaje de error");
-            System.out.println("El elemento fue localizado usando: " + logoCSSLocatorFalse);
-        } catch (NoSuchElementException elementoNoLocalizado) {
-            Assert.fail("El elemento no fue encontrado con el localizador: " + logoCSSLocatorFalse);
+            System.out.println("El elemento fue localizado usando: " + logoCSSLocatorFalso);
+        }catch (NoSuchElementException elementoNoLocalizado){
+            Assert.fail("El elemento no fue encontrado con el localizador: " + logoCSSLocatorFalso);
         }
     }
 
-    //1. Abrir My Store
+    //1. Abrir MyStore
     //2. Click en Carrito
-    //3. Verificar que la URL que se abre es: http://automationpractice.com/index.php?controller=order
+    //3. Verificar que la URL que se abre es http://automationpractice.com/index.php?controller=order
 
     @Test
-    public void testURLCart() {
+    public void testVerificaURLCarrito(){
         openMyStore();
-        WebElement cart = _childWebDriver.findElement(By.cssSelector(cartCSSLocator));
-        cart.click();
-        String cartURLActual = _childWebDriver.getCurrentUrl();
-        String cartURLExpected = "http://automationpractice.com/index.php?controller=order";
-        Assert.assertEquals(cartURLActual, cartURLExpected,
-                "Error: URL actual and expected do not match.");
+        try{
+            WebElement carrito = _childWebDriver.findElement(By.cssSelector(carritoCSSLocator));
+            Assert.assertTrue(carrito.isDisplayed(), "No se encontró el carrito");
+            carrito.click();
+        }catch (NoSuchElementException elementoNoLocalizado){
+            Assert.fail("El elemento no fue encontrado con el localizador: " + carritoCSSLocator);
+        }
+        String carritoURLActual = _childWebDriver.getCurrentUrl();
+        String carritoURLExpected = "http://automationpractice.com/index.php?controller=order";
+        Assert.assertTrue(carritoURLExpected.equals(carritoURLActual),
+                "La URL actual: " + carritoURLActual + " es diferente a la esperada: " + carritoURLExpected);
+        System.out.println("La URL actual: " + carritoURLActual + " es igual a la esperada: " + carritoURLExpected);
     }
 
-    //Tarea para el proximo Sabado 2 de abril 2022
-    //Test para localizar la caja de busqueda de My Store testSearchBoxPresent()
-    //Requerimiento al entrar a My Store la caja de busqueda (Search text box) debe estar visible
-    //Leer la Guia Rapida de GitHub (Crear branch y subir al repo)
-    //Mandar usuario a Jenny por correo o por Slack
+    //Tarea para el próximo sábado 2 de abril 2022
+    //Test para localizar la caja de búsqueda de MyStore testSearchBoxPresent()
+    //Requerimiento: al entrar a MyStore la caja de búsqueda (search text box) debe estar visible
+    //Leer la guía rápida de GitHub (Crear branch y subir al repo)
+    //Mandar usuario a Jenni por correo o por slack
 
-    private void openMyStore() {
+    //Tarea
+    @Test
+    public void testSearchBoxPresent(){
+        //Apertura de MyStore
+        openMyStore();
+        //Búsqueda del objeto por selector
+        try{
+            WebElement searchTextBox = _childWebDriver.findElement(By.cssSelector(searchTextBoxLocator));
+            Assert.assertTrue(searchTextBox.isDisplayed(), "No se encontró la caja de búsqueda");
+            System.out.println("La caja de texto con localizador: " + searchTextBoxLocator + " fue encontrada");
+        }catch (NoSuchElementException elementoNoLocalizado){
+            Assert.fail("El elemento no fue encontrado con el localizador: " + searchTextBoxLocator);
+        }
+    }
+
+    private void openMyStore(){
         _childWebDriver.get(urlMyStore);
-        System.out.println("El navegador ha abierto la URL: " + urlMyStore);
+        System.out.println("El navegador ha abierto la URL " + urlMyStore);
     }
 }
