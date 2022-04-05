dd84e20caba3100e68270043ea9fa901496223cb
Jennifer Ramirez <jxr20920@dexcom.com>
2022-04-04 21:40:43 -0500

    Ejercicios y tarea del 4 de abril 2022 refactorizacion de codigo

--- src/main/java/Steps/SearchResultsSteps.java
+++ src/main/java/Steps/SearchResultsSteps.java
@@ -20,6 +20,7 @@ public class SearchResultsSteps extends BaseSteps {
 
     public void clearSearchTextBox() {
         WebElement searchTextBox = _searchResultsPage.getSearchTextBox();
+        System.out.println("Limpiando Search text box");
         searchTextBox.clear();
         clickSearchButton();
     }
@@ -29,16 +30,19 @@ public class SearchResultsSteps extends BaseSteps {
         WebElement searchTextBox = _searchResultsPage.getSearchTextBox();
         //Metodo para obtener un atributo del HTML
         String value = searchTextBox.getAttribute("value");
+        System.out.println("Obteniendo el atributo value del search text box: " + value);
         return value;
     }
 
     public void clickSearchButton() {
         WebElement searchButton = _searchResultsPage.getBtnSearch();
+        System.out.println("Clic en Search button (lupa)");
         searchButton.click();
     }
 
     public void sendSearchText(String text) {
         WebElement searchTextBox = _searchResultsPage.getSearchTextBox();
+        System.out.println("Enviando texto a search text box: " + text);
         searchTextBox.sendKeys(text);
     }
 }
--- src/test/java/SearchResultsTests.java
+++ src/test/java/SearchResultsTests.java
@@ -1,7 +1,13 @@
 import Steps.SearchResultsSteps;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import sun.util.resources.cldr.sv.CalendarData_sv_SE;
 
-public class SearchResultsTests extends BaseTest{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SearchResultsTests extends BaseTest {
     SearchResultsSteps _searchResultsSteps = new SearchResultsSteps(webDriver);
 
     @Test
@@ -19,4 +25,28 @@ public class SearchResultsTests extends BaseTest{
         //Step 5: Verify textbox is clear
         textBoxValue = _searchResultsSteps.getTextBoxValue();
     }
+
+    @Test
+    public void testMultipleSearch() {
+        //List of strings to search: blouse, shirt, dress
+        List<String> searchTerms = Arrays.asList("blouse", "shirt", "dress");
+        //String[] searchTerms1 = new String[] {"blouse", "shirt", "dress"};
+
+        _searchResultsSteps.openMyStore();
+
+        for (String term : searchTerms) {
+            //Send String to search text box
+            _searchResultsSteps.sendSearchText(term);
+            //Click search button
+            _searchResultsSteps.clickSearchButton();
+            //Verify text box value is as String
+            String currentSearchValue = _searchResultsSteps.getTextBoxValue();
+            System.out.println("El valor del text box de busqueda es: " + currentSearchValue);
+            Assert.assertEquals(currentSearchValue, term);
+            _searchResultsSteps.clearSearchTextBox();
+        }
+    }
+
+    //Tarea 04 abril 2022
+    //Obtener total de resultados (WebElement)
 }
