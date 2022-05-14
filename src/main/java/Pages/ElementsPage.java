4f54973e9ec636004996791a4965b1be14537436
Heriberto Fabian Santiago <fabiansantiago87@gmail.com>
2022-05-14 09:21:44 -0500

    final moment

---
+++ src/main/java/Pages/ElementsPage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+import Steps.BaseSteps;
+import org.openqa.selenium.support.ui.Select;
+
+public class ElementsPage extends BasePage {
+
+    private String dropDownColorsId = "oldSelectMenu";
+
+    public Select  getDropDownColors(){
+        return t
+    }
+}
---
+++ src/test/java/ElementTest.java
@@ -0,0 +1,4 @@
+public class ElementTest extends BaseTest{
+
+
+}
