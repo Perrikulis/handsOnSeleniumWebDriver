b9a1fcf6edd3617e0721233911c23af2c77074da
CynRomero <67176179+CynRomero@users.noreply.github.com>
2022-05-15 02:49:26 -0500

    Mis cambios S1012

--- src/main/java/Pages/MyStoreTShirtsPage.java
+++ src/main/java/Pages/MyStoreTShirtsPage.java
@@ -1,17 +1,14 @@
 package Pages;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-
 import java.util.List;
 
-public class MyStoreWomenPage extends BasePage {
-    WebDriver webDriverMyStore;
+public class MyStoreTShirtsTests extends BasePage {
 
-    public MyStoreWomenPage(WebDriver webDriverMyStore) {
+
+    public MyStoreTShirtsTests (WebDriver webDriverMyStore) {
         this.webDriverMyStore = webDriverMyStore;
     }
 
--- src/main/java/Steps/MyStoreTShirtsSteps.java
+++ src/main/java/Steps/MyStoreTShirtsSteps.java
@@ -1,6 +1,6 @@
 package Steps;
 
-import Pages.MyStoreWomenPage;
+import Pages.MyStoreTShirtsTests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class MyStoreWomenSteps extends BaseSteps{
 
-    MyStoreWomenPage womenPage = PageFactory.initElements(_webDriver, MyStoreWomenPage.class);
+    MyStoreTShirtsTests womenPage = PageFactory.initElements(_webDriver, MyStoreTShirtsTests.class);
     private final String womenCategorySelector = "#block_top_menu ul li a[title='Women']";
 
     public MyStoreWomenSteps(WebDriver webDriver) {
