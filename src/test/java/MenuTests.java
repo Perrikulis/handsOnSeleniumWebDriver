724ad2b3dc8c2273934fd86732dd3d75ec85584c
zapatax6 <37126916+zapatax6@users.noreply.github.com>
2022-05-16 21:19:37 -0500

    dresses category added to MenuTest

--- src/test/java/MenuTests.java
+++ src/test/java/MenuTests.java
@@ -1,3 +1,4 @@
+import Steps.CategoryDressesSteps;
 import Steps.CategoryTshirtsSteps;
 import Steps.CategoryWomenSteps;
 import org.testng.Assert;
@@ -6,6 +7,7 @@ import org.testng.annotations.Test;
 public class MenuTests extends BaseTest{
     CategoryWomenSteps _categoryWomenSteps = new CategoryWomenSteps(webDriver);
     CategoryTshirtsSteps _categoryTshirtsSteps = new CategoryTshirtsSteps(webDriver);
+    CategoryDressesSteps _categoryDressesSteps = new CategoryDressesSteps(webDriver);
 
     @Test(description = "Tests para probar la categoria WOMEN del proyecto final")
     public void testSR1010(){
@@ -23,6 +25,23 @@ public class MenuTests extends BaseTest{
         Assert.assertEquals(_categoryWomenSteps.verifyWomenText2(),"shoes, accessories, printed t-shirts, feminine dresses, women's jeans!");
         //Step 7: Validate Women 7 products
         Assert.assertEquals(_categoryWomenSteps.verifyWomenResults(),"There are 7 products.");
+    }
+    @Test(description = "Test del Requerimiento SR1011, Menú Dresses")
+    public void testSR1011() {
+        //Step 1: Abrir My Store
+        _categoryDressesSteps.openMyStore();
+        //Step 2: Click in Dresses Menu
+        _categoryDressesSteps.clickDressesMenu();
+        //Step 3: Validate Dresses category URL
+        Assert.assertTrue(_categoryDressesSteps.verifyURLCategoryDresses());
+        //Step 4: Validate Dresses Banner
+        Assert.assertTrue(_categoryDressesSteps.verifyDressesBanner());
+        //Step 5: Validate Dresses Text
+        Assert.assertEquals(_categoryDressesSteps.verifyDressesText(), "Find your favorites dresses from our wide choice of evening, casual or summer dresses!");
+        //Step 6: Validate Dresses Text 2
+        Assert.assertEquals(_categoryDressesSteps.verifyDressesText2(), "We offer dresses for every day, every style and every occasion.");
+        //Step 7: Validate Dresses 7 products
+        Assert.assertEquals(_categoryDressesSteps.verifyDressesResults(), "There are 5 products.");
     }
 
     @Test(description = "Tests para probar la categoria T-shirts del proyecto final")
